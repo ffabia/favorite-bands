e36c18acdb85ecf2e492b7cfc81ccf686755b863
Frances Moore <ffabiamoore@gmail.com>
2017-11-01 20:22:07 -0400

    Adding files

---
+++ main.controller.js
@@ -0,0 +1,8 @@
+
+
+var app= angular.module("myApp", []);
+app.controller("MainCtrl", function($scope){
+    $scope.bands = ['Alabama Shakes', ' Arrested Development', 'Deltron 3030', 'The Fugees', 'Fulanito'];
+});
+
+
